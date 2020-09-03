57bf285caa3e0ad944c50d17c06c2e3fb70d12db
ryanp <ryanpanimation@gmail.com>
2020-09-02 23:56:11 -0500

    first half complete

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,26 +6,39 @@ level operating system functionality.
 """
 
 import sys
+
+print("SYS Module\n")
+
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print(sys.argv())
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print(f"OS platform: {sys.platform}")
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
+print(
+    f"Python version: {sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+)
 
 import os
+
+print("\nOS Module\n")
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print(f"process ID: {os.getpid()}")
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(f"working directory: {os.getcwd()}")
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print(f"login: {os.environ.get('USER')}")
+print(f"login opt2:{os.getenv('USER')}")
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -16,25 +16,15 @@ has the following keys:
 """
 
 waypoints = [
-    {
-        "lat": 43,
-        "lon": -121,
-        "name": "a place"
-    },
-    {
-        "lat": 41,
-        "lon": -123,
-        "name": "another place"
-    },
-    {
-        "lat": 43,
-        "lon": -122,
-        "name": "a third place"
-    }
+    {"lat": 43, "lon": -121, "name": "a place"},
+    {"lat": 41, "lon": -123, "name": "another place"},
+    {"lat": 43, "lon": -122, "name": "a third place"},
 ]
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+newPoint = {"lat": 40, "lon": -120, "name": "this place"}
+waypoints.append(newPoint)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +32,10 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]["name"] = "not a real place"
+waypoints[0]["lon"] = -130
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for point in waypoints:
+    print(point["lat"], point["lon"], point["name"])
